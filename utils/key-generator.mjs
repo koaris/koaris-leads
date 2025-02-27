341da1531ee7bd5501d7dbddee52c5fe4b7b7957
Guilherme Salviano <guilherme.salviano12@outlook.com>
2025-02-27 16:27:31 -0300

    hotfix(action): mailchimp envs

--- utils/key-generator.mjs
+++ utils/key-generator.mjs
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const webhookSecret = process.env.WEBHOOK_SECRET;
-const body = '{"optin": true, "type": "touch", "message": "Olá, tudo bem? Gostaria de saber se está tudo bem. Tomou água e tem feito exercícios regularmente? Me avisa, att.", "user": {"firstName": "Joe", "lastName": "Doe", "phone": "11911234567", "email": "joe.doe@test.com", "souce": "Koaris LP - Contacts", "country": "BR"}}';
+const body = '{"optin": true, "type": "touch", "message": "Olá, tudo bem? Gostaria de saber se está tudo bem. Tomou água e tem feito exercícios regularmente? Me avisa, att.", "user": {"firstName": "Joe", "lastName": "Doe", "phone": "11911234567", "email": "joe.doe@suamaeaquelaursa.com", "souce": "Koaris LP - Contacts", "country": "BR"}}';
 
 let hmac = crypto.createHmac('sha256', webhookSecret);
 let signature = hmac.update(body).digest('hex');
